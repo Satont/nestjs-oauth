038f491631ab6ed71433785b5a1e5cd3c0c9ff22
Satont <satontworldwide@gmail.com>
2023-11-11 05:04:13 +0300

    third commit

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,6 @@ import * as session from 'express-session';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  // app.enableCors();
 
   app.use(cookieParser());
   app.use(
